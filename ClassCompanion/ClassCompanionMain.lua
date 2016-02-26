809a00442b6522b2760ac80a02ace0664daff33d
MaxyDawg56 <camm.loren@yahoo.com>
2016-02-25 21:41:52 -0600

    Added addon

---
+++ ClassCompanion/ClassCompanionMain.lua
@@ -0,0 +1,183 @@
+-- Login Event --
+local Login_Frame = CreateFrame("Frame")
+Login_Frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+Login_Frame:SetScript("OnEvent", 
+  function(self, event, ...)
+    print("ClassCompanion has loaded successfully!")
+
+    if hasLoggedIn == nil then
+      -- Druid class --
+      if playerClass == "Druid" then
+        -- Calls wait function for login message --
+        -- Change integer to increase or decrease delay (in seconds) -- 
+        wait(5, fillerWaitFunction , druidFirstLoginMessage1, whisperSound)
+        wait(10, fillerWaitFunction , druidFirstLoginMessage2, whisperSound)
+        if playerLevel < 3 then
+          wait(15, fillerWaitFunction, druidWrathMessage1, whisperSound)
+          wait(20, fillerWaitFunction, druidWrathMessage2, whisperSound)
+        end
+      end
+      hasLoggedIn = true
+    else
+      wait(5, fillerWaitFunction, druidRegLoginMessage1, whisperSound)
+      wait(10, fillerWaitFunction, druidRegLoginMessage2, whisperSound)
+    end
+end)
+
+-- OnKill Event --
+local Kill_frame = CreateFrame("Frame");
+Kill_frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
+Kill_frame:SetScript("OnEvent", 
+  function(self, event, ...)
+    -- DRUID CLASS --
+    if(event=="COMBAT_LOG_EVENT_UNFILTERED" and playerClass == "Druid") then
+      local time, type, sourceGUID, sourceName, sourceFlags, destGUID, destName, destFlags=...
+      -- If player kills something --
+      if(type=="PARTY_KILL" and sourceFlags == playerName)then
+        -- If the kills hit the cap --
+        if ClassCompanionKillCounter >= ClassCompanionKillMessageCap then
+            -- Sets the kill limit on the message event --
+            ClassCompanionKillMessageCap = math.random(10, 15)
+            -- Sets counter back to 1 after cap is hit --
+            ClassCompanionKillCounter = 1
+            ClassCompanionKillMessageChoice = math.random(1,5)
+            -- Message redundancy check --
+            while (ClassCompanionKillMessageRedundancy == ClassCompanionKillMessageChoice) do
+              ClassCompanionKillMessageChoice = math.random(1,5)
+            end
+            -- Message 1 --
+            if(ClassCompanionKillMessageChoice == 1) then
+              wait(0, fillerWaitFunction, druidKillMessage1, whisperSound)
+            end
+             -- Message 2 --
+            if(ClassCompanionKillMessageChoice == 2) then
+              wait(0, fillerWaitFunction, druidKillMessage2, whisperSound)
+            end
+             -- Message 3 --
+            if(ClassCompanionKillMessageChoice == 3) then
+              wait(0, fillerWaitFunction, druidKillMessage3, whisperSound)
+            end
+             -- Message 4 --
+            if(ClassCompanionKillMessageChoice == 4) then
+              wait(0, fillerWaitFunction, druidKillMessage4, whisperSound)
+            end
+             -- Message 5 --
+            if(ClassCompanionKillMessageChoice == 5) then
+              wait(0, fillerWaitFunction, druidKillMessage5, whisperSound)
+            end
+        end
+        -- Increments the kills --
+        ClassCompanionKillCounter = ClassCompanionKillCounter + 1
+        -- Sets redundancy --
+        ClassCompanionKillMessageRedundancy = ClassCompanionKillMessageChoice
+      end
+    end
+end)
+
+-- Level up --
+local Level_Up_Frame = CreateFrame("Frame")
+Level_Up_Frame:RegisterEvent("PLAYER_LEVEL_UP")
+Level_Up_Frame:SetScript("OnEvent", 
+  function(self, event, ...)
+    local arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9 = ...
+    playerLevel = UnitLevel("player")
+    -- DRUID CLASS --
+    if (playerClass == "Druid") then
+      print(levelUpMessage)
+    end
+    wait(0, fillerWaitFunction, nil, levelUpSound)
+end)
+
+-- Learned spell --
+local Learned_Spell_Frame = CreateFrame("Frame")
+Learned_Spell_Frame:RegisterEvent("LEARNED_SPELL_IN_TAB")
+Learned_Spell_Frame:SetScript("OnEvent",
+  function(self, event, ...)
+    local spellID, tabID = ...
+    -- DRUID CLASS --
+    if (playerClass == "Druid") then
+      -- Moonfire --
+      if (spellID == 8921) then
+        wait(3, fillerWaitFunction, druidMoonfireMessage1, whisperSound)
+        wait(8, fillerWaitFunction, druidMoonfireMessage2, whisperSound)
+      end
+      -- Rejuvenation --
+      if (spellID == 774) then
+        wait(3, fillerWaitFunction, druidRejuvMessage1, whisperSound)
+        wait(8, fillerWaitFunction, druidRejuvMessage2, whisperSound)
+      end
+      -- Cat Form --
+      if (spellID == 768) then
+        wait(3, fillerWaitFunction, druidCatFormMessage1, whisperSound)
+        wait(8, fillerWaitFunction, druidCatFormMessage2, whisperSound)
+        wait(11, fillerWaitFunction, druidCatFormMessage3, whisperSound)
+        wait(14, fillerWaitFunction, druidCatFormMessage4, whisperSound)
+      end
+      -- Feline Grace --
+      if (spellID == 125972) then
+        wait(17, fillerWaitFunction, druidFelineGraceMessage1, whisperSound)
+        wait(20, fillerWaitFunction, druidFelineGraceMessage2, whisperSound)
+        wait(23, fillerWaitFunction, druidFelineGraceMessage2, whisperSound)
+      end
+      -- Ferocious Bite --
+      if (spellID == 22568) then
+        wait(26, fillerWaitFunction, druidFerociousBiteMessage1, whisperSound)
+      end
+      -- Prowl --
+      if (spellID == 5215) then
+        wait(29, fillerWaitFunction, druidProwlMessage1, whisperSound)
+      end
+      -- Shred --
+      if (spellID == 5221) then
+        wait(32, fillerWaitFunction, druidShredMessage1, whisperSound)
+        wait(35, fillerWaitFunction, druidShredMessage2, whisperSound)
+      end
+    end
+end)
+
+-- Death --
+local Death_Frame = CreateFrame("Frame")
+Death_Frame:RegisterEvent("PLAYER_DEAD")
+Death_Frame:SetScript("OnEvent", 
+  function(self, event, ...)
+    wait(0, fillerWaitFunction, druidDeathMessage1, whisperSound, deathSound)
+  end)
+
+-- Filler function for wait --
+function fillerWaitFunction(str, ...)
+  if str ~= nil then
+    print (str)
+  end
+  PlaySoundFile(..., "Master")
+end
+
+-- Wait function -- 
+local waitTable = {};
+local waitFrame = nil;
+function wait(delay, func, ...)
+  if(type(delay)~="number" or type(func)~="function") then
+    return false;
+  end
+  if(waitFrame == nil) then
+    waitFrame = CreateFrame("Frame","WaitFrame", UIParent);
+    waitFrame:SetScript("onUpdate",function (self,elapse)
+      local count = #waitTable;
+      local i = 1;
+      while(i<=count) do
+        local waitRecord = tremove(waitTable,i);
+        local d = tremove(waitRecord,1);
+        local f = tremove(waitRecord,1);
+        local p = tremove(waitRecord,1);
+        if(d>elapse) then
+          tinsert(waitTable,i,{d-elapse,f,p});
+          i = i + 1;
+        else
+          count = count - 1;
+          f(unpack(p));
+        end
+      end
+    end);
+  end
+  tinsert(waitTable,{delay,func,{...}});
+  return true;
+end
---
+++ ClassCompanion/ClassCompanionMessages.lua
@@ -0,0 +1,70 @@
+local Login_Frame = CreateFrame("Frame")
+Login_Frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+Login_Frame:SetScript("OnEvent", 
+function(self, event, ...)
+	playerLevel = UnitLevel("player")
+    -- Counter to keep track of kills --
+    ClassCompanionKillCounter = 1
+    -- Cap for the kills, at the cap a message will play --
+    ClassCompanionKillMessageCap = 0
+    -- Ensures no message will play twice in a row --
+    ClassCompanionKillMessageRedundancy = 0
+
+    --Sound Files--
+    whisperSound = "Interface\\AddOns\\ClassCompanion\\whisper.ogg"
+    deathSound = "Interface\\AddOns\\ClassCompanion\\BBDeathFull.ogg"
+    levelUpSound = "Interface\\AddOns\\ClassCompanion\\NYEEESS.ogg"
+
+    --Player Info--
+	playerName = UnitName("player")
+	playerRealm = GetRealmName()
+	playerClass = UnitClass("player")
+
+	--General--
+	levelUpMessage = ("|cffFF7D0AOstmarian whispers: Congratulations " .. playerName .. ", you are " .. (100 - playerLevel - 1) .. " levels from your maximum potential!"):format()
+
+	--Druid--
+	if playerClass == "Druid" then
+		--Login--
+        druidFirstLoginMessage1 = ("|cffFF7D0AOstmarian whispers: Salutations " .. playerName .. " of the realm " .. playerRealm .. ".|r" ):format()
+        druidFirstLoginMessage2 = ("|cffFF7D0AOstmarian whispers: I Ostmarian, fabled archdruid of the ancient realm, am here to guide you.|r" ):format()
+        druidRegLoginMessage1 = ("|cffFF7D0AOstmarian whispers: Back again I see. I have been awaiting your arrival since last we met.|r"):format()
+        druidRegLoginMessage2 = ("|cffFF7D0AOstmarian whispers: Come, there is much to be done my friend.|r"):format()
+        -- Death --
+        druidDeathMessage1 = ("|cffFF7D0AOstmarian whispers: Ah... well fret not my dear friend, the fabrics of this world will build you anew.|r"):format()
+        --Lvl less than 3--
+		druidWrathMessage1 = ("|cffFF7D0AOstmarian whispers: Currently your only spell is Wrath, its baseline cast time is 2 seconds and does moderate damage.|r"):format()
+		druidWrathMessage2 = ("|cffFF7D0AOstmarian whispers: You'll be continuously casting this until you gain other abilities.|r"):format()
+		--Kills--
+		-- The '|cffXXXXXX', '|r', and ':format()' are for message coloring (The X's represent a hex value)
+        -- To edit the message, change the text between the formating
+        druidKillMessage1 = ("|cffFF7D0AOstmarian whispers: What do you think you're doing?! This is no way a druid behaves!|r"):format()
+        druidKillMessage2 = ("|cffFF7D0AOstmarian whispers: For the love of Elune, stop this bloodshed!|r"):format()
+        druidKillMessage3 = ("|cffFF7D0AOstmarian whispers: Well let's hope that their passing will contribute to a greater tomorrow, yes?|r"):format()
+        druidKillMessage4 = ("|cffFF7D0AOstmarian whispers: Did they start the assault or did you? I don't pay much attention to fighting.|r"):format()
+        druidKillMessage5 = ("|cffFF7D0AOstmarian whispers: Oh again with the killing, what would D.E.H.T.A. think of this?|r"):format()
+        --LearnSpells--
+        -- Moonfire
+        druidMoonfireMessage1 = ("|cffFF7D0AOstmarian whispers: You have learned Moonfire! This is a fairly powerful spell at a low level and is instant cast!|r"):format()
+        druidMoonfireMessage2 = ("|cffFF7D0AOstmarian whispers: This is a DoT (Damage over time) spell, meaning its damage is spread out over time. Many DoTs on a target will cause them to be overwhelmed.|r"):format()
+        -- Rejuvenation
+        druidRejuvMessage1 = ("|cffFF7D0AOstmarian whispers: You have learned Rejuvenation, your first healing spell!|r"):format()
+        druidRejuvMessage2 = ("|cffFF7D0AOstmarian whispers: This is a instant cast HoT (Healing over time) spell, it functions the same as a DoT but may only be cast on friendly unit to heal them. |r"):format()
+        -- Cat form
+        druidCatFormMessage1 = ("|cffFF7D0AOstmarian whispers: You have learned Cat Form, your first shapeshift form!|r"):format()
+        druidCatFormMessage2 = ("|cffFF7D0AOstmarian whispers: It allows you to shapeshift into, well, a cat! Doing so allows different abilities and restricts others.|r"):format()
+        druidCatFormMessage3 = ("|cffFF7D0AOstmarian whispers: In this form you use some abilities to gain combo points and others to spend them.|r"):format()
+        druidCatFormMessage4 = ("|cffFF7D0AOstmarian whispers: (P.S. Don't use the gnomes as a scratching post, they don't respond well to it.)|r"):format()
+        -- Feline Grace
+        druidFelineGraceMessage1 = ("|cffFF7D0AOstmarian whispers: One of the first moves gained from Cat Form is Feline Grace!|r"):format()
+        druidFelineGraceMessage2 = ("|cffFF7D0AOstmarian whispers: While in Cat Form, Feline Grace enables you to take less damage from falling.|r"):format()
+        druidFelineGraceMessage3 = ("|cffFF7D0AOstmarian whispers: You don't need to use this move, it's a passive and always activated while in Cat Form.|r"):format()
+        -- Ferocious Bite
+        druidFerociousBiteMessage1 = ("|cffFF7D0AOstmarian whispers: The next move gained from Cat Form is Ferocious Bite. It does higher damage based on higher combo points, up to 5.|r"):format()
+        -- Prowl
+        druidProwlMessage1 = ("|cffFF7D0AOstmarian whispers: Cat Form also gives you Prowl, an ability that grants you stealth until you're hit or attack. Only usable out of combat.|r"):format()
+        -- Shred 
+        druidShredMessage1 = ("|cffFF7D0AOstmarian whispers: The last move gained from Cat Form for now is Shred. This moves grants you a combo point.|r"):format()
+        druidShredMessage2 = ("|cffFF7D0AOstmarian whispers: Using Shred in stealth grants more damage and doubles the crit chance, also does more damage against targets with bleed on them. |r"):format()
+	end
+end)
